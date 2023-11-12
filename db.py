117c1ba0d244a81c61423444dec1e483c17c26c4
Mikael Alvian Rizky <mikael.alvian@ui.ac.id>
2023-11-12 12:33:51 +0700

    add table user, child, and height, also setup the local database

---
+++ db.py
@@ -0,0 +1,38 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from main import app
+
+# app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://randstack:randstackDev321@34.122.224.222:5432/machine_learning"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    email = db.Column(db.String(120))
+    password = db.Column(db.String(60))
+
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now())
+
+class Child(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    name = db.Column(db.String(200))
+    birth_date = db.Column(db.Date)
+
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now())
+
+class ChildHeight(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    child_id = db.Column(db.Integer)
+    height = db.Column(db.Float)
+    taken_date = db.Column(db.DateTime, default=db.func.now())
+
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now())
--- main.py
+++ main.py
@@ -1,101 +1,19 @@
-import os
-from datetime import datetime
-import cv2
-from flask import Flask, request
-from rembg import remove
-from skimage.measure import regionprops
+from flask import Flask
 from flask_cors import CORS
-
-import base64
-from io import BytesIO
-from PIL import Image
-
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
 CORS(app)
 
 @app.route('/')
 def hello():
     return 'Welcome!'
 
-@app.route('/api/v1/predict', methods=['POST'])
-def predictv1():
-    # Get the file from post request
-    image_base64 = request.json['image']
-
-    # Extract the MIME type and base64 data
-    mime_type, base64_string = image_base64.split(';base64,')
-    image_format = mime_type.split('/')[-1]
-
-    current_dateTime = datetime.now()
-    filename = f'{current_dateTime.microsecond}.{image_format}'
-
-    # Save the file to ./images
-    save_path = os.path.join(os.getcwd(), 'images', filename)
-
-    # Decode the base64 string into bytes
-    bytes_decoded = base64.b64decode(base64_string)
-
-    # Open the image from bytes and save it to a file
-    with Image.open(BytesIO(bytes_decoded)) as img:
-        img.save(save_path)
-
-    # Read the image
-    image = cv2.imread(save_path)
-    image = cv2.resize(image,(880, 1320))
-
-    # Remove background
-    image = remove(image)
-
-    # Thersholding
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _, thresh = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY)
-    
-    props = regionprops(thresh)
-    for p in props:
-        majL = p.axis_major_length
-
-    metrics_per_pixel = majL/92.5
-    tinggi = majL/8.93
-
-    os.remove(save_path)
-    return f"{round(tinggi,1)}"
-
-@app.route('/api/v2/predict', methods=['POST'])
-def predictv2():
-    uploaded_file = request.files['image']
-
-    # Check if the 'image' field was sent in the request
-    if uploaded_file:
-        current_dateTime = datetime.now()
-        filename = f'{current_dateTime.microsecond}.{"png"}'
-
-        # Save the file to ./images
-        save_path = os.path.join(os.getcwd(), 'images', filename)
-
-        uploaded_file.save(save_path)
-
-        # Read the image
-        image = cv2.imread(save_path)
-        image = cv2.resize(image,(880, 1320))
-
-        # Remove background
-        image = remove(image)
-
-        # Thersholding
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        _, thresh = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY)
-        
-        props = regionprops(thresh)
-        for p in props:
-            majL = p.axis_major_length
-
-        metrics_per_pixel = majL/92.5
-        tinggi = majL/8.93
-
-        os.remove(save_path)
-        return f"{round(tinggi,1)}", 200
-    else:
-        return 'Image not found in the request', 400
+try:
+    from controller.auth_controller import *
+    from controller.height_controller import *
+    from db import *
+except Exception as e:
+    print(e)
 
 if __name__ == "__main__":
     # This is used when running locally only. When deploying to Google App
